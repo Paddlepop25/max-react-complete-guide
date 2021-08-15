8815b33a764c7c93c056bf035ee1bd94c4020eef
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-08-15 14:09:52 +0800

    end of using styled components

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -11,6 +12,10 @@ const Button = styled.button`
 
   &:focus {
     outline: none;
+  }
+
+  @media (min-width: 768px) {
+    width: auto;
   }
 
   &:hover,
