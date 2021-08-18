2e7983452afb8377313a747496b592edd1797c73
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-08-18 08:04:18 +0800

    provided dummy function in auth-context for IDE hint

--- src/components/context/store/auth-context.js
+++ src/components/context/store/auth-context.js
@@ -3,6 +3,7 @@ import React from 'react';
 // set default state in createContext
 const AuthContext = React.createContext({
   contextIsLoggedIn: false,
+  onContextLoggedOut: () => {}, // just dummy function here for IDE hint in Navigation.js
 });
 
 // need to provide Context to be used in other components (once this Context wraps around them)
