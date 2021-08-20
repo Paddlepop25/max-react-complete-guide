fff22ddc0c7c95baf36f1c8657ac89f51f7a84a2
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-08-20 12:57:09 +0800

    added CartButton video 133

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import mealImage from '../../assets/meals.jpeg'; // can rename it in import xxx
 import classes from './Header.module.css';
-
+import HeaderCartButton from './HeaderCartButton';
 const Header = () => {
   return (
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={mealImage} alt='Table full of delicious food' />
