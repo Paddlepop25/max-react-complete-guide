ade573bb8b27ef1d9c4f6ad03daf65e7f1577f59
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-08-20 11:10:05 +0800

    reset src folder for FoodOrderApp - video 130

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import { AuthContextProviderCustom } from './components/context/store/auth-context';
 
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <AuthContextProviderCustom>
-    <App />
-  </AuthContextProviderCustom>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
