ee8b0386a8b865513229d35316e030c2851a22dc
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-08-20 17:07:22 +0800

    add Cart Reducer: video 143

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import CardContext from './cart-context';
 
-const CartProvider = (props) => {
-  const addItemToCardHandler = (item) => {};
+const initialCartState = {
+  items: [],
+  totalAmount: 0,
+};
 
-  const removeItemFromCartHandler = (id) => {};
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.payload); // concat gives a new array
+    const updatedTotalAmount = (state.totalAmount =
+      action.payload.price * action.payload.amount);
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return initialCartState;
+};
+
+const CartProvider = (props) => {
+  // 1st array elem - cart snapshot
+  // 2nd array elem - function to dispatch action
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    initialCartState
+  );
+
+  const addItemToCardHandler = (item) => {
+    dispatchCartAction({ type: 'ADD', payload: item });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: 'REMOVE', id: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCardHandler,
     removeItem: removeItemFromCartHandler,
   };
