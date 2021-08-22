c7e4bffaddacebc1cd5717ede192fb72567be983
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-08-22 17:12:25 +0800

    add UserFinder, filter users based on input: Video 166

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,11 +1,18 @@
 import { Fragment, useState, useEffect } from 'react';
-
+import classes from './UserFinder.module.css';
 import Users from './Users';
+
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+];
 
 const UserFinder = () => {
   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
   const [searchTerm, setSearchTerm] = useState('');
 
+  // search for "Max" not "max"
   useEffect(() => {
     setFilteredUsers(
       DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
@@ -18,7 +25,9 @@ const UserFinder = () => {
 
   return (
     <Fragment>
-      <input type='search' onChange={searchChangeHandler} />
+      <div className={classes.finder}>
+        <input type='search' onChange={searchChangeHandler} />
+      </div>
       <Users users={filteredUsers} />
     </Fragment>
   );
