5b6ed04d842c65559f817939558c1bb4c5fcec06
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-08-22 19:47:14 +0800

    added isLoading state: Video 177

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+import { code } from 'esutils';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch('https://swapi.dev/api/films/');
     const data = await response.json();
     const transformedMovies = data.results.map((movieData) => {
@@ -18,6 +21,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -26,7 +30,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {isLoading && <code>Loading results...</code>}
+        {!isLoading && movies.length === 0 && <code>No movies found</code>}
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
       </section>
     </React.Fragment>
   );
