c7c89f76b6417b7c5363072ecedec417ea95fd2c
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-08-22 22:04:14 +0800

    refactor useCounter custom hook to use for ForwardCounter.js and BackwardCounter.js

--- src/components/ForwardCounter.js
+++ src/components/ForwardCounter.js
@@ -5,7 +5,9 @@ import Card from './Card';
 
 const ForwardCounter = () => {
   // only logic will be shared from custom hooks, not state
-  const counter = useCounter(); // will have value of the returned 'counter' from useCounter hook
+  // will have value of the returned 'counter' from useCounter hook
+  // default is true (forwards = true) so no need to add anything in useCounter()
+  const counter = useCounter();
 
   return <Card>{counter}</Card>;
 };
--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -2,16 +2,20 @@ import React, { useState, useEffect } from 'react';
 
 // custom hook must start with 'use'
 // only logic will be shared from custom hooks, not state
-const useCounter = () => {
+const useCounter = (forwards = true) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if (forwards) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [forwards]); // this forwards dependency will changee
 
   return counter;
 };
