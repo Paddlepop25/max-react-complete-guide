a06921fd1f7631ed9b9e6edefcf8193cc63b95af
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-08-22 14:08:39 +0800

    useCallback to store functions that will never change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Button from './components/UI/Button/Button';
 import './App.css';
 import DemoOutput from './components/DemoOutput/DemoOutput';
 
+// useCallback hook allows us to store functions
+// the stored functions should not be re-created with every with every execution
+// but it will point to the same function stored in memory
+
 function App() {
   const [toggleShowText, setToggleShowText] = React.useState(false);
 
-  const onClickHandler = () => {
+  // if we sure this function will never change, we can use useCallback to store it
+  // also need array of dependencies like useEffect
+  const onClickHandler = useCallback(() => {
     setToggleShowText((prevState) => !prevState);
-  };
+  }, []); // [] --> any state, props, context
 
   console.log('App');
 
