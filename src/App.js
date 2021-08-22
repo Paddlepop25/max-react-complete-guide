1b24afacdf6ce92aac6cfd01edb2105f1bc0217d
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-08-22 14:38:37 +0800

    reset project for Class Components: Video 164

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import React, { useCallback } from 'react';
-import Button from './components/UI/Button/Button';
-import './App.css';
-import DemoOutput from './components/DemoOutput/DemoOutput';
-
-// useCallback hook allows us to store functions
-// the stored functions should not be re-created with every with every execution
-// but it will point to the same function stored in memory
+import Users from './components/Users';
 
 function App() {
-  const [toggleShowText, setToggleShowText] = React.useState(false);
-
-  // if we sure this function will never change, we can use useCallback to store it
-  // also need array of dependencies like useEffect
-  const onClickHandler = useCallback(() => {
-    setToggleShowText((prevState) => !prevState);
-  }, []); // [] --> any state, props, context
-
-  console.log('App');
-
   return (
-    <div className='app'>
-      <h1>Hi there!</h1>
-      <DemoOutput show={false} />
-      <Button onClick={onClickHandler}>Toggle paragraph</Button>
+    <div>
+      <Users />
     </div>
   );
 }
--- src/components/DemoOutput/MyParagraph.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const MyParagraph = (props) => {
-  console.log('MP');
-  return <p>{props.children}</p>;
-};
-
-export default MyParagraph;
