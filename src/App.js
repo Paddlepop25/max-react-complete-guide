5b752409745d5d29655fd8074c83be0b8583b4d3
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-08-22 21:47:51 +0800

    restart proj for building Custom hooks:

--- src/App.js
+++ src/App.js
@@ -1,91 +1,12 @@
-import React, { useState, useEffect, useCallback } from 'react';
-
-import MoviesList from './components/MoviesList';
-import AddMovie from './components/AddMovie';
-import './App.css';
+import React from 'react';
+import BackwardCounter from './components/BackwardCounter';
+import ForwardCounter from './components/ForwardCounter';
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  const fetchMoviesHandler = useCallback(async () => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://max-udemy-http-b52eb-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json'
-      );
-      if (!response.ok) {
-        throw new Error('Something went wrong!');
-      }
-
-      const data = await response.json();
-      console.log(data);
-      // openingText: "Woohoo"
-      // releaseDate: "18/05/1990"
-      // title: "My first movie"
-
-      const loadedMovies = [];
-      for (const key in data) {
-        loadedMovies.push({
-          id: key,
-          title: data[key].title,
-          openingText: data[key].openingText,
-          releaseDate: data[key].releaseDate,
-        });
-      }
-
-      setMovies(loadedMovies);
-    } catch (error) {
-      setError(error.message);
-    }
-    setIsLoading(false);
-  }, []);
-
-  useEffect(() => {
-    fetchMoviesHandler();
-  }, [fetchMoviesHandler]);
-
-  async function addMovieHandler(movie) {
-    // could add try catch but we not doing it here
-    const response = await fetch(
-      'https://max-udemy-http-b52eb-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json',
-      {
-        method: 'POST',
-        body: JSON.stringify(movie),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-    const data = await response.json();
-    console.log(data);
-  }
-
-  let content = <p>Found no movies.</p>;
-
-  if (movies.length > 0) {
-    content = <MoviesList movies={movies} />;
-  }
-
-  if (error) {
-    content = <p>{error}</p>;
-  }
-
-  if (isLoading) {
-    content = <p>Loading...</p>;
-  }
-
   return (
     <React.Fragment>
-      <section>
-        <AddMovie onAddMovie={addMovieHandler} />
-      </section>
-      <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-      </section>
-      <section>{content}</section>
+      <ForwardCounter />
+      <BackwardCounter />
     </React.Fragment>
   );
 }
