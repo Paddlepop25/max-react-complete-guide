a76a71492841826200a478b9771fd07313ebb482
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-08-22 17:39:50 +0800

    use Context (not the hook) in Class based component: Video 168

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,14 +1,11 @@
 import { Fragment, useState, useEffect, Component } from 'react';
+import UsersContext from '../store/users-context';
 import classes from './UserFinder.module.css';
 import Users from './Users';
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
-
 class UserFinder extends Component {
+  static contextType = UsersContext;
+
   constructor() {
     super();
     this.state = {
@@ -20,13 +17,13 @@ class UserFinder extends Component {
   // example if we fetching DUMMY_USERS from database
   componentDidMount() {
     // send http request when component first mount
-    this.setState({ filteredUsers: DUMMY_USERS });
+    this.setState({ filteredUsers: this.context.users });
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.searchTerm !== this.state.searchTerm) {
       this.setState({
-        filteredUsers: DUMMY_USERS.filter((user) =>
+        filteredUsers: this.context.users.filter((user) =>
           user.name.includes(this.state.searchTerm)
         ),
       });
