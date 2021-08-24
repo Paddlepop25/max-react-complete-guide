c6d87b75ccc4914eb752a237f43a16a678707d6f
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-08-24 09:12:40 +0800

    refactor to check if form is valid: Video 204

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -10,6 +10,12 @@ const SimpleInput = (props) => {
   const enteredNameIsValid = enteredName.trim() !== '';
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
+
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
@@ -51,7 +57,7 @@ const SimpleInput = (props) => {
         )}
       </div>
       <div className='form-actions'>
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
