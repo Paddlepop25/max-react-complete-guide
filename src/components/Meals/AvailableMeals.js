bf45e366b15c5b6c1da04194492fc440d38c154d
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-09-05 16:42:43 +0800

    handing loading and errors in form: Video 216

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import classes from './AvailableMeals.module.css';
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
+  // don't use async with useEffect(async () ..) because it shouldn't return a promise
+  // instead put the async inside a function in useEffect. workaround
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        'https://max-udemy-http-b52eb-default-rtdb.asia-southeast1.firebasedatabase.app/meals.jsodn'
+      );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong');
+      }
+
+      const responseData = await response.json(); // await returns a promise
+      // console.log(responseData);
+
+      const loadedMeals = [];
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      console.log(loadedMeals);
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    // see video why prefer not to use try catch here with await
+    // this is traditional promise way of handling an error inside of a promise
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.mealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.mealError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
