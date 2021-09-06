e6f8873e66957da50b1344613a86479ebe16bc1d
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-09-06 12:36:58 +0800

    alternative way of switch case in counterReducer

--- src/components/store/index.js
+++ src/components/store/index.js
@@ -5,35 +5,66 @@ const initialState = {
   showCounter: true,
 };
 
+// NEVER mutate exisiting state. so always make a copy first
 const counterReducer = (state = initialState, action) => {
-  if (action.type === 'increment') {
-    return {
-      ...state,
-      counter: state.counter + 1,
-    };
-  }
+  // if (action.type === 'increment') {
+  //   return {
+  //     ...state,
+  //     counter: state.counter + 1,
+  //   };
+  // }
 
-  if (action.type === 'increase') {
-    return {
-      ...state,
-      counter: state.counter + action.payload,
-    };
-  }
+  // if (action.type === 'increase') {
+  //   return {
+  //     ...state,
+  //     counter: state.counter + action.payload,
+  //   };
+  // }
 
-  if (action.type === 'decrement') {
-    return {
-      ...state,
-      counter: state.counter - 1,
-    };
-  }
+  // if (action.type === 'decrement') {
+  //   return {
+  //     ...state,
+  //     counter: state.counter - 1,
+  //   };
+  // }
 
-  if (action.type === 'toggle') {
-    return {
-      ...state,
-      showCounter: !state.showCounter,
-    };
+  // if (action.type === 'toggle') {
+  //   return {
+  //     ...state,
+  //     showCounter: !state.showCounter,
+  //   };
+  // }
+  // return initialState;
+
+  switch (action.type) {
+    case 'increment': {
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+    }
+    case 'decrement': {
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    }
+    case 'toggle': {
+      return {
+        ...state,
+        showCounter: !state.showCounter,
+      };
+    }
+    case 'increase': {
+      return {
+        ...state,
+        counter: state.counter + 5,
+      };
+    }
+    default: {
+      return initialState;
+    }
   }
-  return initialState;
 };
 
 const store = createStore(counterReducer);
