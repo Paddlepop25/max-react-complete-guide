68aaae8ab81c263d7b4d23837504141bab9ea089
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-09-08 21:30:38 +0800

    get params of dynamic route url with useParams: Video 271

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
+  const params = useParams(); // key value pairs; key is dynamic value of the route url
+
+  // example /product-detail/:productId for http://localhost:3000/product-detail/vvv
+  // key=productId, value=vvv
+  // {productId: "vvv"}
+  console.warn(params);
+
   return (
     <section>
       <h2>Product Detail</h2>
+      <p>{params.productId}</p>
     </section>
   );
 };
