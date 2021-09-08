2245fd4d69ab34738bc5aa472f903953b5679113
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-09-08 21:47:42 +0800

    added nested route to see 2 diff things on same component/page based on url: Video 273

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,16 @@
+import { Route } from 'react-router-dom';
+
 const Welcome = () => {
-  return <h1>The Welcome Page</h1>;
+  return (
+    <section>
+      <h1>The Welcome Page</h1>
+      <Route path='/welcome/new-user'>
+        <p>Welcome new user! 🍭</p>
+      </Route>
+    </section>
+  );
 };
 
 export default Welcome;
+
+// <Route path='/welcome/new-user'> can be used for other users to see extra info on this component/page
