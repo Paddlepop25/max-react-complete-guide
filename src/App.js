56d27be83d4ad2d7002b7a87a51411c03b996be2
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-09-08 20:45:41 +0800

    basic routing setup. pages, <Route>: Video 267

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Route } from 'react-router-dom';
+import Products from './pages/Products';
+import Welcome from './pages/Welcome';
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Route path='/welcome'>
+        <Welcome />
+      </Route>
+      <Route path='/products'>
+        <Products />
+      </Route>
     </div>
   );
 }
 
 export default App;
+
+/**
+ * our-domain-name/welcome -----> Welcome component
+ * our-domain-name/products -----> Products component
+ */
