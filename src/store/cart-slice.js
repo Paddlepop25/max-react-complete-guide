d490605c500dbb43bd9018a3e9e4d0f392b540b9
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-09-08 15:59:42 +0800

    thunk: put http fetch request logic in cart-slice and make leaner component: Video 258

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { uiActions } from './ui-slice';
 
 const initialCartState = {
   items: [],
@@ -7,7 +8,10 @@ const initialCartState = {
 
 /* in normal redux never manipulate state directly
 but below is okay because reduxtoolkit uses 3rd party library IMER 
-to make a copy of state so we can update it in an immutable way
+to make a copy of state so we can update it in an immutable way.
+
+reduxtoolkit will automatically create action creators for us with the methods
+e.g. addItemToCart to have {type: 'addItemToCart', payload: ...}
 */
 const cartSlice = createSlice({
   name: 'cart',
@@ -45,6 +49,53 @@ const cartSlice = createSlice({
     },
   },
 });
+
+// thunk is a function that delays an action until later
+// thunk is a function that return another function
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data!',
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        'https://max-udemy-http-b52eb-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed');
+      }
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success...',
+          message: 'Sent cart data successfully!',
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed :(',
+        })
+      );
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 
