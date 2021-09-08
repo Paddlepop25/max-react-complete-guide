53854c3d4f9f08af635a22da16b77429d24c68bd
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-09-08 21:13:04 +0800

    changed Link to NavLink to display activeClassName class when link is active: Video 269

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,18 +1,24 @@
-import { Link } from 'react-router-dom';
-
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import classes from './MainHeader.module.css';
 
 const MainHeader = () => {
   return (
-    <header>
-      <ul>
-        <li>
-          <Link to='/welcome'>Welcome</Link>
-        </li>
-        <li>
-          <Link to='/products'>Products</Link>
-        </li>
-      </ul>
+    <header className={classes.header}>
+      <nav>
+        <ul>
+          <li>
+            <NavLink activeClassName={classes.active} to='/welcome'>
+              Welcome
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName={classes.active} to='/products'>
+              Products
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
