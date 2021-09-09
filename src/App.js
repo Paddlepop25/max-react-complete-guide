9c6c7d16afd4a87860caae410abf91d2460e56c6
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-09-09 21:42:05 +0800

    branch new project to practise routing: Video 275

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
-import { Route, Switch, Redirect } from 'react-router-dom';
-import Products from './pages/Products';
-import Welcome from './pages/Welcome';
-import MainHeader from './components/MainHeader';
-import ProductDetail from './pages/ProductDetail';
-
-/**
- * Switch is to find the first match for the route from top to bottom, then will stop
- * exact will match only the exact path
- */
 function App() {
   return (
     <div>
-      <MainHeader />
-      <main>
-        <Switch>
-          <Route path='/' exact>
-            <Redirect to='/welcome' />
-          </Route>
-          <Route path='/welcome'>
-            <Welcome />
-          </Route>
-          <Route path='/products' exact>
-            <Products />
-          </Route>
-          <Route path='/products/:productId'>
-            <ProductDetail />
-          </Route>
-        </Switch>
-      </main>
+      
     </div>
   );
 }
 
 export default App;
-
-/**
- * our-domain-name/welcome -----> Welcome component
- * our-domain-name/products -----> Products component
- * our-domain-name/product-detail/anyDynamicValue -----> ProductDetail component
- */
