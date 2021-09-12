955cec74e4e4143585646181816b3b96345bd0ff
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-09-12 14:58:18 +0800

    add React.lazy and suspense to pages in app.js: Video 292

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
+import React, { Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import Quotes from './pages/Quotes';
-import QuoteDetail from './pages/QuoteDetail';
-import NewQuote from './pages/NewQuote';
 import Layout from './components/layout/Layout';
-import NotFoundPage from './pages/NotFoundPage';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+// this will load component ONLY when it is needed, not downloaded in advance
+const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
+const NotFoundPage = React.lazy(() => import('./pages/NotFoundPage'));
+const Quotes = React.lazy(() => import('./pages/Quotes'));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <Redirect to='/quotes' />
-        </Route>
-        <Route path='/quotes' exact>
-          <Quotes />
-        </Route>
-        <Route path='/quotes/:quoteId'>
-          <QuoteDetail />
-        </Route>
-        <Route path='/new-quote'>
-          <NewQuote />
-        </Route>
-        <Route path='*'>
-          <NotFoundPage />
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className='centered'>
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/quotes' />
+          </Route>
+          <Route path='/quotes' exact>
+            <Quotes />
+          </Route>
+          <Route path='/quotes/:quoteId'>
+            <QuoteDetail />
+          </Route>
+          <Route path='/new-quote'>
+            <NewQuote />
+          </Route>
+          <Route path='*'>
+            <NotFoundPage />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
