d1e86ed1a718d6289a97b6f9f12e90ef7fcc7920
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-10-12 21:32:48 +0800

    primitive types: Video 395

---
+++ basics.ts
@@ -0,0 +1,13 @@
+// Primitives: number, string, boolean, null, undefined
+// more complex types: arraus. objects
+// function types, parameters
+
+// Primitives
+let age: number;
+age = 12;
+
+let userName: string;
+userName = 'Max';
+
+let isInstructor: boolean;
+isInstructor = true;
