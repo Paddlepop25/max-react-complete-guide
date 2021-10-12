2222d7f51005f72fa50e2e9b73e45559d49b243c
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-10-12 16:22:13 +0800

    mock fetch with simulated response as in async.js

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import Async from './Async';
 
+/**
+ * Note: in testing we don't want to actually send real requests to servers
+ * because it may insert real data or create unnecessary network traffic or avoid if server is down.
+ * So we mock the sending request
+ */
+
 describe('test suite for Async component', () => {
   test('renders posts if request succeeds', async () => {
+    // mock fetch send request in Async.js and get back array with result
+    window.fetch = jest.fn();
+
+    // window.fetch should mock the fetch to get the resolved value that we use in Async.js to render out li
+    // json because response.json(). it is a function that returns a promise (hence async) with values of id + title
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: 'p1', title: 'first post' }],
+    });
+
     render(<Async />);
 
     // https://www.w3.org/TR/html-aria/#docconformance
