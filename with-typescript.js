27e27bbd67a4245ced0f3964b1bae6b6db4523b5
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-10-12 16:41:51 +0800

    initial commit of typescript

---
+++ with-typescript.js
@@ -0,0 +1,5 @@
+function adda(a, b) {
+    return a + b;
+}
+var resulta = adda(2, 5);
+console.log('>', resulta);
---
+++ with-typescript.ts
@@ -0,0 +1,7 @@
+function adda(a: number, b: number) {
+  return a + b;
+}
+
+const resulta = adda(2, 5);
+
+console.log('>', resulta);
