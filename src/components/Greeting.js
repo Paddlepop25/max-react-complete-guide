f831b196e502ee6c657bf522faaca9e8b8538e6e
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-10-12 14:42:51 +0800

    added click button tests; his way my way: Video 386

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Greeting = () => {
+  const [changeText, setChangedText] = useState(false);
+
+  const changeTextHandler = () => {
+    setChangedText(true);
+  };
+
   return (
     <>
       <h1>Hello 🌏 World!!!!!</h1>
-      <code>Its good to see ya!</code>
+      {!changeText && <code>It's good to see ya!</code>}
+      {changeText && <code>Changed!</code>}
+      <br />
+      <br />
+      <button
+        data-testid='greeting__change-text-button'
+        onClick={changeTextHandler}
+      >
+        Change Text
+      </button>
     </>
   );
 };
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Greeting from './Greeting';
 
 // do 3 things
@@ -27,5 +28,53 @@ describe('Greeting component test suite', () => {
       exact: false,
     });
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  // {exact: false is not strict}
+  test('text before button is clicked', () => {
+    render(<Greeting />);
+    const textBeforeClicked = screen.getByText('good to see ya', {
+      exact: false,
+    });
+    expect(textBeforeClicked).toBeInTheDocument();
+  });
+
+  // Linda
+  test('text change after button is clicked', () => {
+    const { queryByTestId, queryByText, debug } = render(<Greeting />);
+    const changeTextButton = queryByTestId('greeting__change-text-button');
+
+    expect(changeTextButton).toHaveTextContent('Change Text');
+    expect(queryByText("It's good to see ya!")).toBeInTheDocument();
+
+    fireEvent.click(changeTextButton);
+
+    expect(queryByText("It's good to see ya!")).not.toBeInTheDocument();
+    expect(queryByText('Changed!')).toBeInTheDocument();
+  });
+
+  // Max
+  test('text change to "Changed!" after button is clicked', () => {
+    render(<Greeting />);
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    const outputElement = screen.getByText('Changed!');
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  // Max
+  test('text does not render "It\'s good to see ya!" after button is clicked', () => {
+    render(<Greeting />);
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    // getByText would throw error if not found so change to queryByText
+    // queryByText would return null if not found
+    const initialElement = screen.queryByText('good to see ya!', {
+      exact: false,
+    });
+    // expect(initialElement).not.toBeInTheDocument(); // also works
+    expect(initialElement).toBeNull();
   });
 });
