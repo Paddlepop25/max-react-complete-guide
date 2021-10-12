2fdf8b15aab5119c2821ed39861f11b76ababec2
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-10-12 10:32:18 +0800

    finished useContext for favourite

--- src/context/products-context.js
+++ src/context/products-context.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 export const ProductsContext = React.createContext({
   products: [],
+  toggleFav: (id) => {},
 });
 
 export default (props) => {
@@ -16,7 +17,7 @@ export default (props) => {
       id: 'p2',
       title: 'Blue T-Shirt',
       description: 'A pretty blue t-shirt.',
-      isFavorite: false,
+      isFavorite: true,
     },
     {
       id: 'p3',
@@ -31,8 +32,24 @@ export default (props) => {
       isFavorite: false,
     },
   ]);
+
+  const toggleFavorite = (productId) => {
+    setProductsList((currentProdList) => {
+      const prodIndex = currentProdList.findIndex((p) => p.id === productId);
+      const newFavStatus = !currentProdList[prodIndex].isFavorite;
+      const updatedProducts = [...currentProdList];
+      updatedProducts[prodIndex] = {
+        ...currentProdList[prodIndex],
+        isFavorite: newFavStatus,
+      };
+      return updatedProducts;
+    });
+  };
+
   return (
-    <ProductsContext.Provider value={{ products: productsList }}>
+    <ProductsContext.Provider
+      value={{ products: productsList, toggleFav: toggleFavorite }}
+    >
       {props.children}
     </ProductsContext.Provider>
   );
