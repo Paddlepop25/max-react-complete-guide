faec009b2cf4868a3f58a028fc260223689dcd09
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-10-12 09:57:29 +0800

    removed redux, give useContext hook to display products

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 
 import Card from '../UI/Card';
 import './ProductItem.css';
 import { toggleFav } from '../../store/actions/products';
 
-const ProductItem = props => {
-  const dispatch = useDispatch();
-
+const ProductItem = (props) => {
   const toggleFavHandler = () => {
-    dispatch(toggleFav(props.id));
+    // dispatch(toggleFav(props.id));
   };
 
   return (
     <Card style={{ marginBottom: '1rem' }}>
-      <div className="product-item">
+      <div className='product-item'>
         <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
         <p>{props.description}</p>
         <button
