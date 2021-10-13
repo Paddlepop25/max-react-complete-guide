dcedd59b6ab1df6eb6ee533c1f1f9c3ed36fa79b
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-10-13 11:11:13 +0800

    add useRef for form in NewTodo

--- react-ts/src/components/NewTodo.tsx
+++ react-ts/src/components/NewTodo.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const NewTodo = () => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
+    // something?.value = try to get me the value, if its null, store as null
+    // something!.value = only use ! when you sure it will never be null
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      // throw an erorr
+      return;
+    }
   };
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor='text'>Enter todo: </label>
-      <input type='text' id='text' placeholder='e.g buy maggi mee' />
+      <input
+        type='text'
+        id='text'
+        placeholder='e.g buy maggi mee'
+        ref={todoTextInputRef}
+      />
       <button>Add todo</button>
     </form>
   );
