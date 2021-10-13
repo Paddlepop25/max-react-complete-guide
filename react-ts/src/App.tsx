8b41fdfe7c28cae54cc997b9a096745a677e41a8
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-10-13 09:55:13 +0800

    added Todos component

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,7 +1,11 @@
-import './App.css';
+import Todos from './components/Todos';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Todos />
+    </div>
+  );
 }
 
 export default App;
